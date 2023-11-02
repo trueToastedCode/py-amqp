1b53b156038d90d2e28d3b0cbd2c03de282d41b7
trueToastedCode <lennard2hp@gmail.com>
2023-11-02 19:50:33 +0100

    fix import

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import aio_pika
-from ..id import Id
+from Id.index import Id
 
-from base_amqp_access import build_make_amqp_access
+from .base_amqp_access import build_make_amqp_access
 
 make_amqp_access = build_make_amqp_access(aio_pika, Id)
