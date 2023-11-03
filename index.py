1f83a70ebbfbe394abe405eb1a1fdbdd4cd1c174
trueToastedCode <lennard2hp@gmail.com>
2023-11-03 10:41:03 +0100

    fix import

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import logging
 import aio_pika
 import functools
 
-from amqp_callback.index import make_amqp_callback
+from ..amqp_callback.index import make_amqp_callback
 
 from .listen_react_amqp import make_listen_react_amqp
 
